6f0e92d454e3da9e980a563480ba0f84408a1860
Nick Byrne <byrnen8@tcd.ie>
2026-08-18 18:08:29 -0300

    Regenerate readiness after sticky mount rejection

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "e07ba0dd8bdfc7b153b9df81e1084ce1ad643d87a2231ced531c4dc70a6c64bb",
+  localValidationNormalized: "afbdb1e25ffc5aa9c1f593d4fdd1149db6cbf644a3a71e61367dd734f21ab0a4",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "da9d83d318467471069d4b47eb002ec28f914dea21e4a5a26f9e932808ffafeb",
+  sourceInventoryNormalized: "a42f3aa0de7372b4bfa30e6e465cd85bb9c5241659814f25fc6ef7d0d7f6d3a0",
 });
 /* stage4-readiness-anchor-end */
 
