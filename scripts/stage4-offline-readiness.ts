0b677e4ef5551597296b0bb3b670dde08da89f23
Nick Byrne <byrnen8@tcd.ie>
2026-08-18 13:50:58 -0300

    Regenerate readiness after namespace bind errno

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "86b59edbeff430d35b3ec8cedafa80e3e72a0085932b40148b9498a41510a664",
+  localValidationNormalized: "22bd579d2575e8b2d19f4c58f374fd5f10bfe3e02de7d07f3658b4cb61cd5e92",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "c172d29344aaf9e93269f83a3e38134aa5f70571cc2e4ba9775da585bcf87064",
+  sourceInventoryNormalized: "f1ab876c9f3737353e795b31da37ecc612179d0bd1bc68961e870e58a51518d9",
 });
 /* stage4-readiness-anchor-end */
 
