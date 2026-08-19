22b803f6b6ade665311315c31e01ee0ec3e0f1fe
Nick Byrne <byrnen8@tcd.ie>
2026-08-19 13:47:43 -0300

    Regenerate readiness after unretained daemon correction

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "9652f39c6de9987b9bbb957bfbd0dbe4eecc0b2ad0b3c550c629de5f6a08dfb2",
+  localValidationNormalized: "fdd5886e8224f8d26784903190a133c1d2805cf4252db56aba39bc33fcc2be17",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "d583304cdaa020940e1ebde3cfaa3eee9dbf827c5b5b0b75104fd0f85f6643e2",
+  sourceInventoryNormalized: "3a334f3ca516a421e3c8821148d32e02ff996fb5cc3922a4c0cc5b5551479a82",
 });
 /* stage4-readiness-anchor-end */
 
