0c9850b10b23c77e8a893554f398b1fb5a109063
Nick Byrne <byrnen8@tcd.ie>
2026-08-20 00:35:37 -0300

    Regenerate readiness after observer deduplication

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "4e4be88a9711935df9ec387ba819aab431a4dcf4923f627247427043e218245b",
+  localValidationNormalized: "387b612661e340111351ded4a713d51da55cb3a97c801c7a7e9ae35c51dc61de",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "1dcb467bd0d281d3bb4d526963ec0e8f7ac2aba7e81f1d4d60f2d24308cc0217",
+  sourceInventoryNormalized: "602233f23244ac7d21e232c20ab0b7495ac9a312705f52000d2ac69beeb627f5",
 });
 /* stage4-readiness-anchor-end */
 
