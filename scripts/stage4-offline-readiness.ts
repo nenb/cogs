82d1fe01269632b16c8fac61a42a0f075fbb4a54
Nick Byrne <byrnen8@tcd.ie>
2026-08-19 21:06:53 -0300

    Regenerate readiness after NFT ownership recovery fixes

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "129030c2c8d089595990d5eb0d0d3ec6552b384b68ddc30383fa3d48a17b121e",
+  localValidationNormalized: "f3a88a7b5f47c11a4e7021d7f3062f11af7f47d1a3c9cde32bd8a4b65ddd6d75",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "6a0ab222ea2dd401cf3fafc82fc2a39bb9d84b149f7d2a1d142c87b07e9c952f",
+  sourceInventoryNormalized: "64968651829000c49efe86d7820c934fc092dd1cd9278ca41fc9c15ef5157f04",
 });
 /* stage4-readiness-anchor-end */
 
