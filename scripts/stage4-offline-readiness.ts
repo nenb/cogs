e658f55792e95352cf2c550c315cc3b9d3aae796
Nick Byrne <byrnen8@tcd.ie>
2026-08-22 16:05:55 -0300

    Revert "Regenerate readiness for corrected KVM control"
    This reverts commit 84853f7817378739d7ebfe20e4d7593c4e1d42d7.

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "2489deaddade787d28c7288b9a4e7e3a68f03c7399bb73ca3ce1f3e936daf582",
+  localValidationNormalized: "a0a635ed0ef08e4014355671924d0e759676378ff900f03e92b4de906fb1eac8",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "26477907ab2505022b88c855b1f9fbabcede6c5f948e555fbd91f8b58439f29c",
+  sourceInventoryNormalized: "bd57de63dd38d72ba49554477cc7d541553f53ac0bbdc768348d1704bac7f1ff",
 });
 /* stage4-readiness-anchor-end */
 
