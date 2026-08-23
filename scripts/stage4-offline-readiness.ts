f5a056feb613dfdab5892939ec16ab1dc18bef7e
Nick Byrne <byrnen8@tcd.ie>
2026-08-22 22:37:34 -0300

    Regenerate readiness for netns contract

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "c2c1cf1fded232119eb8e26ec569cb5ec7052d923e866d82237f3c273905c60a",
+  localValidationNormalized: "6d1fcf6d1f0ca70886cf184c412765f04413ca564a3d4ac7a4de19bd7ec4ac71",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "2ea7b1a6fb0bd780986f9776b515d35a355b1bb58d9251c4b07f6c60daff9c1e",
+  sourceInventoryNormalized: "c3ad1e72373f051f70c88160eab5da066d3c5b7da2d944c7514b00f4191e6e52",
 });
 /* stage4-readiness-anchor-end */
 
