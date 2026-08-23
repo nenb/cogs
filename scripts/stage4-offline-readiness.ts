36bfa11a5da930d3b36eba4a388aba9ed85119d1
Nick Byrne <byrnen8@tcd.ie>
2026-08-23 07:34:55 -0300

    Regenerate readiness for build-substage control

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "6998571f57db3f4d0b4240cd430e2928f22f6118d663b814ac436aa1d36e483e",
+  localValidationNormalized: "435a7b47d918d278d14d7dc1ca12e1905b01ac60f39dc3d116a84f713bba49b8",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "b210adab3be92b3572bf0e4ec1971519c5bf5c9928ca08d5682beee4bf816318",
+  sourceInventoryNormalized: "4b564d0cccd7ae3e41ec865bd47f410467068479a33406925381c1fc7117d992",
 });
 /* stage4-readiness-anchor-end */
 
