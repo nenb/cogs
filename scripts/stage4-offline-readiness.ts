e07ae27d53614b4cb1ab1c6dba869d6102a44716
Nick Byrne <byrnen8@tcd.ie>
2026-08-26 12:18:49 -0300

    Regenerate readiness after OpenSSH fd correction

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "5a04568d14b359695a897f7a1fa99a578653baaaac778e0e15d58914805cbc3b",
+  localValidationNormalized: "aaf600f24497b492d3f69a73fe383449b24914ea63eefc946a0e32150edda18b",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "bc94f756909e8fe176956bd42bedd1c31517d76aa1fd99c06062ab6fb6166416",
+  sourceInventoryNormalized: "594f0dbf89d51902304fc7c9d31f059ed88110d7d8880ba6b0cd507472ae8a76",
 });
 /* stage4-readiness-anchor-end */
 
