5c97db8190e7589fa8ece8e47ef596d62f7e85aa
Nick Byrne <byrnen8@tcd.ie>
2026-08-27 23:20:49 -0300

    Regenerate readiness after rootfs release correction

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "7cdd1827d87482e48fb02b293120f2192202a2a6d223d29608f431520b1242ac",
+  localValidationNormalized: "401d050dcef017e92b3689cc3be5795f69ade8868c0df06bc50184d2b4bffd95",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "cec695f8b0a6d32101335d075aa514738561291ddedb87bca1e73d105d3e4680",
+  sourceInventoryNormalized: "1e43d7c4560233cbd30ab8b4280d13689a62e8b304be25f9f511818dd73af519",
 });
 /* stage4-readiness-anchor-end */
 
