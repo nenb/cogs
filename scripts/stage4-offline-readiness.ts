c5f2d681b727e5a550a9bfca7d9164da01c8b2d4
Nick Byrne <byrnen8@tcd.ie>
2026-09-01 04:52:57 -0300

    Import fixed rootfs builder in producer

--- scripts/stage4-offline-readiness.ts
+++ scripts/stage4-offline-readiness.ts
@@ -238,10 +238,10 @@ export const STAGE4_READINESS_EXPECTED_ARTIFACTS = Object.freeze({
   runtimePins: "1e683ef6513f9f86f7eaead0fd64d949f037afd06043882eb1b6514aa5c4a145",
   values: "c689236c57e1eab668f8bf504e148245cc23a652b529d1aaab20ef8d4e0fdc7a",
   authenticatedRuntimeArtifacts: "cd87233d2f3e6be755e78ce63a4b3e85c088fe75997dfe22943bb890317844e3",
-  localValidationNormalized: "8fe388c02dbc804376a829c5bd79a4b48682a5f2ed619bae25d4f30ffda9bc27",
+  localValidationNormalized: "e0e8ea62c1f3c7d90dba5215a127693f882ac2877da6a9f099b3d5d826643a88",
   renderReceipt: "491c7963c00873ee6429cb3917c2ae1316e83b5905257b1abc8c60a4464541cf",
   schemaInventory: "ca8a324816c2ab45f96aafb0c916b3b12d4b1b15eeb51bf63ac69b45b143e683",
-  sourceInventoryNormalized: "189368f9e715bd9973f78f062f68211ef5f949ed791c8dc2bb71eed2a71a7ec4",
+  sourceInventoryNormalized: "15bd5376ef16c3ad94a2f0856d9dfea52f8341abd6b7ae3bedec6aa54003eb4b",
 });
 /* stage4-readiness-anchor-end */
 
