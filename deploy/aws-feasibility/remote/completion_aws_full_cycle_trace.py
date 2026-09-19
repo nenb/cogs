4932863485ca2016a526b0df3f2239fde00ba944
Nick Byrne <byrnen8@tcd.ie>
2026-09-19 10:21:32 -0300

    Hotpatch diagnostic input creation umask

--- deploy/aws-feasibility/remote/completion_aws_full_cycle_trace.py
+++ deploy/aws-feasibility/remote/completion_aws_full_cycle_trace.py
@@ -58,6 +58,10 @@ class _TracingOwners:
 
         def traced(*args, **kwargs):
             _emit("owner-call-start", owner_call=name)
+            previous_umask = None
+            if name == "create_inputs":
+                previous_umask = os.umask(0o022)
+                _emit("diagnostic-hotpatch", owner_call=name, umask="0022")
             try:
                 result = value(*args, **kwargs)
             except BaseException as error:
@@ -65,6 +69,9 @@ class _TracingOwners:
                       exception_type=type(error).__name__, message=str(error)[:1000])
                 _exception(error)
                 raise
+            finally:
+                if previous_umask is not None:
+                    os.umask(previous_umask)
             _emit("owner-call-passed", owner_call=name)
             return result
 
